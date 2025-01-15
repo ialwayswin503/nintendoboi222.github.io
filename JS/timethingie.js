2f5e1ac4c780e310fc20e16ad46754beedee1ead
Nintendoboi2/Nintendoboi22 <131544102+Nintendoboi22@users.noreply.github.com>
2025-01-15 12:34:48 -0500

    Update timethingie.js

--- JS/timethingie.js
+++ JS/timethingie.js
@@ -7,6 +7,7 @@ window.onload = function() {
     document.getElementById('time-counter').textContent = formatTime(savedTime);
   }, 1000);
 };
+
 function resetTime() {
   localStorage.setItem('savedTime', 0);
   document.getElementById('time-counter').textContent = formatTime(0);
@@ -14,14 +15,14 @@ function resetTime() {
 
 function formatTime(seconds) {
   var days = Math.floor(seconds / 86400);
-  var hours = Math.floor(seconds / 3600);
+  var hours = Math.floor((seconds % 86400) / 3600);
   var minutes = Math.floor((seconds % 3600) / 60);
   seconds = seconds % 60;
-  
+
   var daysDisplay = days > 0 ? days + (days === 1 ? ' day, ' : ' days, ') : '';
   var hoursDisplay = hours > 0 ? hours + (hours === 1 ? ' hour, ' : ' hours, ') : '';
   var minutesDisplay = minutes > 0 ? minutes + (minutes === 1 ? ' minute, ' : ' minutes, ') : '';
-  var secondsDisplay = seconds + ('and ' seconds === 1 ? ' second' : ' seconds');
+  var secondsDisplay = seconds + (seconds === 1 ? ' second' : ' seconds');
 
   return daysDisplay + hoursDisplay + minutesDisplay + secondsDisplay;
 }
