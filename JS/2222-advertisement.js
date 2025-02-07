153f32527e1043cec3994b6a8d56d725cbfbb3e5
Do_Not_Dusturb <152636544+Do-Not-Dusturb@users.noreply.github.com>
2025-02-07 18:30:49 -0500

    I love money

--- JS/2222-advertisement.js
+++ JS/2222-advertisement.js
@@ -1,1 +1,2 @@
 alert("Hey there, thanks for using my site! Have you heard of my new one? Well if you did not then go to http://nintendoboi2222.github.io. (please try to use that more as it will allow me to make more websites!) Thanks!, Nintendoboi2 ");
+window.open("http://nintendoboi2222.github.io");
